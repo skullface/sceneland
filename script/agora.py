9adf46eebe1e5a7e09813316968e2deba8dc5134
skullface <skullface@users.noreply.github.com>
2023-08-13 14:13:55 -0400

    simplify script variable naming

--- script/agora.py
+++ script/agora.py
@@ -23,17 +23,17 @@ all_shows_list = []
 
 for show in shows:
   all_shows_data = {} 
-  artist_elements = show.find_all("h3", class_="carousel_item_title_small")
+  headliner_elements = show.find_all("h3", class_="carousel_item_title_small")
   support_elements = show.find_all("h4", class_="supporting")
   date_element = show.find("span", class_="date")
-  for artist_element in artist_elements:
-    artist_element.text.strip()
-  for support_element in support_elements:
-    support_element.text.strip()
-  if support_element.text.strip() == "":
-    all_shows_data['artist'] = artist_element.text.strip()
+  for headliner in headliner_elements:
+    headliner.text.strip()
+  for support in support_elements:
+    support.text.strip()
+  if support.text.strip() == "":
+    all_shows_data['artist'] = headliner.text.strip()
   else:
-    all_shows_data['artist'] = artist_element.text.strip() + ", " + support_element.text.strip().replace(";", ",")
+    all_shows_data['artist'] = headliner.text.strip() + ", " + support.text.strip().replace(";", ",")
   all_shows_data['date'] = date_element.text.strip().replace("\n", " ").replace("Aug ", "2023-08-").replace("Sep ", "2023-09-").replace("Oct ", "2023-10-").replace("Nov ", "2023-11-").replace("Dec ", "2023-12-").replace("Jan ", "2024-01-").replace("Feb ", "2024-02-").replace("Mar ", "2024-03-").replace("Apr ", "2024-04-").replace("May ", "2024-05-").replace("Jun ", "2024-06-").replace("Jul ", "2024-07-").replace("Mon, ", "").replace("Tue, ", "").replace("Wed, ", "").replace("Thu, ", "").replace("Fri, ", "").replace("Sat, ", "").replace("Sun, ", "").replace(", 2023", "").replace(", 2024", "")
   all_shows_data['venue'] = "Agora"
   all_shows_list.append(all_shows_data)

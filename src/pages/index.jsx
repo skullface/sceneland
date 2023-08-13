7c3bb291cb4e752e252e6f5b486d403e4125cce9
skullface <skullface@users.noreply.github.com>
2023-08-13 13:28:28 -0400

    `.jsx`

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,7 @@ export default function Home({ shows }) {
   return (
     <>
       <Head>
-        <title>216.shows: Music in Cleveland 🤘</title>
+        <title>216.show: Music in Cleveland 🤘</title>
         <meta name='description' content='Concerts coming up at your fav local venues like the Beachland, Agora, Mahall’s, and more' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.png' />
@@ -25,20 +25,22 @@ export default function Home({ shows }) {
       
       <div>
         <header>
-          <h1>216.shows: Music in Cleveland 🤘</h1>
+          <h1>216.show: Music in Cleveland 🤘</h1>
           <p>Concerts coming up at your fav local venues like the Beachland, Agora, Mahall’s, and more.</p>
         </header>
         
         <main>
-          {shows.map((show, i) => (
-            <article key={i}>
-              <h2>{show.artist}</h2>
-              <time dateTime={new Date(show.date)}>
-                {new Date(show.date).toLocaleDateString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
-              </time>
-              <p>{show.venue}</p>
-            </article>
-          )).sort(compareChronological)}
+          <section className='shows'>
+            {shows.map((show, i) => (
+              <article key={i} className=''>
+                <h2>{show.artist}</h2>
+                <time dateTime={new Date(show.date)}>
+                  {new Date(show.date).toLocaleDateString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
+                </time>
+                <p>{show.venue}</p>
+              </article>
+            )).sort(compareChronological)}
+          </section>
         </main>
         
         <footer>
@@ -46,7 +48,7 @@ export default function Home({ shows }) {
             All data is pulled from the venues’ individual websites and aggregated here. No ownership of information is claimed nor implied.
           </p>
           <p>
-            Support your scene. Take care of each other.
+            Support your scene and take care of each other.
           </p>
         </footer>
       </div>

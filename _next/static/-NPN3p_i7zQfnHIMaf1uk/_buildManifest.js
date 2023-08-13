74648c565c24105ae6ddb5ead34271cef09b0e63
skullface <skullface@users.noreply.github.com>
2023-08-13 07:34:15 +0000

    Deploying to gh-pages from @ skullface/sceneland@4d0099cd2343721707886b6169c00570788f09a8 🚀

---
+++ _next/static/-NPN3p_i7zQfnHIMaf1uk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-1ad4393bea690e62.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

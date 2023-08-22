831605bb2fe86c54b4a9fee9d0a67ff8be20961b
skullface <skullface@users.noreply.github.com>
2023-08-21 22:00:07 -0400

    merge to .js object instead of .json file

--- script/merge.py
+++ script/merge.py
@@ -1,17 +1,26 @@
 import glob
 import json
+import sh
+import os
 
 read_files = glob.glob('src/data/venues/*.json')
-shows_all = []
+all_shows = []
 
-for f in read_files:
-  with open(f, 'rb') as infile:
-    shows_all.append(json.load(infile))
+for file in read_files:
+  with open(file, 'rb') as infile:
+    all_shows.append(json.load(infile))
 
 shows_json = []
-for json_file in shows_all:
+for json_file in all_shows:
   shows_json += json_file
 
-shows_merged = open('src/data/shows.json', 'w')
-json.dump({ 'shows': shows_json }, shows_merged, indent=2)
+shows_merged = open('src/data/allShows.js', 'w')
+json.dump(shows_json, shows_merged, indent=2)
 shows_merged.close()
+
+sh.sed('-i.tmp', '1s/.*/' + 'export const allShows = [' + '/', 'src/data/allShows.js')
+
+if os.path.isfile('src/data/allShows.js.tmp'):
+  os.remove('src/data/allShows.js.tmp')
+else:
+  print("Nothing to delete")

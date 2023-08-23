5a7846c0f0584fecad5398556ad2f3926450b0f5
skullface <skullface@users.noreply.github.com>
2023-08-23 19:55:07 -0400

    update styling a lil bit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,13 +20,13 @@ export const getStaticProps = async () => {
 export function VenueFilter({ venues, selectedVenues, onVenueToggle }) {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className='group dropdown-button w-auto px-4 py-2 font-medium rounded-full text-base bg-zinc-950 hover:bg-zinc-800 transition text-zinc-50 dark:bg-zinc-50 dark:text-zinc-800 border border-zinc-400 dark:border-white dark:hover:bg-zinc-200 focus:outline-none focus:ring focus:ring-indigo-500/50'>
+      <DropdownMenuTrigger className='group dropdown-button select-none w-auto px-4 py-2 font-medium rounded-md text-base bg-zinc-950 hover:bg-zinc-800 transition text-zinc-50 dark:bg-zinc-100 dark:hover:bg-white dark:text-zinc-800 border border-zinc-800 dark:border-white focus:outline-none focus:ring focus:ring-lime-500/75 dark:shadow-[0_16px_24px_-16px_rgba(0,0,0,0.3),inset_0px_1px_0_rgba(0,0,0,0.25) shadow-[0_16px_24px_-16px_rgba(0,0,0,0.3),inset_0px_1px_0_rgba(255,255,255,0.25)]'>
         Select your fav Cleveland venues{' '}
-        <span className='inline-block text-zinc-400 transition group-hover:translate-y-0.5 transform -rotate-90'>
+        <span className='inline-block leading-none text-zinc-400 dark:group-hover:text-zinc-500 group-hover:text-zinc-200 transition-colors transform -rotate-90'>
           &lt;
         </span>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='dark:bg-zinc-50 min-w-[200px] md:min-w-[360px] rounded-xl shadow-lg shadow-black/5 flex flex-col bg-zinc-900'>
+      <DropdownMenuContent className='dropdown-content dark:bg-zinc-50 min-w-[200px] md:min-w-[360px] rounded-xl shadow-lg shadow-black/5 flex flex-col bg-zinc-900'>
         {venues.map((venue) => (
           <DropdownMenuCheckboxItem
             key={venue.replace(/[^\w]+/g, '-').toLowerCase()}
@@ -150,7 +150,7 @@ export default function Home() {
                     <a
                       key={i}
                       href={show.link}
-                      className='group flex flex-col h-full bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 rounded leading-snug p-4 gap-y-2 shadow-sm dark:shadow-md transition hover:dark:border-zinc-700 hover:border-zinc-300 hover:shadow-black/10  focus:outline-none focus:ring focus:ring-indigo-500/25'
+                      className='group flex flex-col h-full bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 rounded leading-snug p-4 gap-y-2 shadow-sm dark:shadow-md transition hover:dark:border-zinc-700 hover:border-zinc-300 hover:shadow-black/10  focus:outline-none focus:ring focus:ring-lime-500/50'
                     >
                       {show.sold_out && (
                         <span className='self-start uppercase border transition rounded-full [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] px-2.5 py-1 text-xs font-semibold tracking-wide whitespace-nowrap dark:text-red-500 text-white border-red-700 dark:border-red-900/75 group-hover:dark:border-red-900 dark:bg-red-950 bg-red-600'>

95dce8bcbc80303f3d75925feb521df0204d5e95
skullface <skullface@users.noreply.github.com>
2023-08-23 22:15:29 -0400

    use fathom for ethical tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('WXSTWRMY', {
+      includedDomains: ['216.show'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }

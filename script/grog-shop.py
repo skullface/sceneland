dc1c018fe753b0a628ab85414b0a64e9f395cc4e
skullface <skullface@users.noreply.github.com>
2023-08-24 22:53:16 -0400

    remove duplicates

--- script/grog-shop.py
+++ script/grog-shop.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup 
 import json
+from collections import OrderedDict
 
 session = requests.Session()
 page = session.get('https://grogshop.gs/index.php', headers={'User-Agent': 'Mozilla/5.0'})
@@ -17,10 +18,28 @@ for show in shows:
   opener = show.find("div", class_="tw-opening-act")
   sold_out = show.find("a", class_="tw_soldout")
   date = show.find("span", class_="tw-event-date")
+  
+  headlinerz = headliner.text.strip().replace("w / ", ", ").replace("w/ ", ", ").replace(" / / ", ", ").replace(" // ", ", ").replace(" / ", ", ").replace(" - ", ": ").replace(" – ", ": ")
+  artists_with_openers = [headlinerz + ", " + opener.text.strip().replace("w/ ", "").replace(" / ", ", ")]
+
+  # Create an ordered dictionary to store unique artist names
+  unique_artists_dict = OrderedDict()
+
+  # Process each entry in the list
+  for entry in artists_with_openers:
+      artists = entry.split(', ')  # Split by comma and space
+      for artist in artists:
+          artist = artist.strip()  # Remove leading/trailing spaces
+          if artist:  # Skip empty entries
+              unique_artists_dict[artist] = None  # Using dict keys to maintain order
+
+  # Convert the ordered dictionary keys back to a list
+  artists_with_openers_clean = list(unique_artists_dict.keys())
+
   if opener.text.strip() == "":
-    all_shows_data['artist'] = [headliner.text.strip().replace("w / ", ", ").replace("w/ ", ", ").replace(" / / ", ", ").replace(" // ", ", ").replace(" / ", ", ")]
+    all_shows_data['artist'] = [headlinerz]
   else:
-    all_shows_data['artist'] = [headliner.text.strip().replace("w / ", ", ").replace("w/ ", ", ").replace(" / ", ", ").replace(" // ", ", ") + ", " + opener.text.strip().replace("w/ ", "").replace(" / ", ", ")]
+    all_shows_data['artist'] = artists_with_openers_clean
   for link_element in show.findAll("a"):
     if link_element.parent == headliner:
       all_shows_data['link'] = link_element["href"]
@@ -31,5 +50,5 @@ for show in shows:
   all_shows_data['venue'] = "Grog Shop"
   all_shows_list.append(all_shows_data)
 
-all_shows_json = json.dumps(all_shows_list, indent=2) 
+all_shows_json = json.dumps(all_shows_list, indent=2)
 print(all_shows_json)

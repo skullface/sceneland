e9c0e0cfc120a5a14205a86e75b8ab3a8c3030ba
skullface <skullface@users.noreply.github.com>
2023-08-26 16:51:38 -0400

    better meta tags

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -3,10 +3,10 @@ import Head from 'next/head'
 export const SiteMeta = () => {
   return (
     <Head>
-      <title>216.show: Music in Cleveland 🤘</title>
+      <title>216.show: Upcoming shows in Cleveland</title>
       <meta
         name='description'
-        content='Concerts coming up at your fav local venues like the Beachland, Agora, Mahall’s, and more'
+        content='Concerts and events coming up at your fav local venues like the Beachland, Agora, Mahall’s, and more'
       />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <link rel='icon' href='/favicon.png' />

93e01ffbacf883eb3c9b72287b799913f13f40fa
skullface <skullface@users.noreply.github.com>
2023-08-25 21:15:02 -0400

    formatting

--- src/components/show-card.tsx
+++ src/components/show-card.tsx
@@ -12,7 +12,7 @@ export const ShowCard: React.FC<ShowCardProps> = ({ show, i }) => {
       <li key={i}>
         <a
           href={show.link}
-          className='group flex h-full flex-col gap-y-2 rounded border border-zinc-200 bg-white p-4 leading-snug shadow-sm transition hover:border-zinc-300 hover:shadow-black/10 focus:outline-none focus:ring focus:ring-lime-500/50 dark:border-zinc-800  dark:bg-zinc-900 dark:shadow-md hover:dark:border-zinc-700'
+          className='group flex h-full flex-col gap-y-2 rounded border border-zinc-200 bg-white p-4 leading-snug shadow-sm transition hover:border-zinc-300 hover:shadow-black/10 focus:outline-none focus:ring focus:ring-lime-500/50 dark:border-zinc-800 dark:bg-zinc-900 dark:shadow-md hover:dark:border-zinc-700'
         >
           {show.sold_out && (
             <span className='self-start whitespace-nowrap rounded-full border border-red-700 bg-red-600 px-2.5 py-1 text-xs font-semibold uppercase tracking-wide text-white transition [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] dark:border-red-900/75 dark:bg-red-950 dark:text-red-500 group-hover:dark:border-red-900'>

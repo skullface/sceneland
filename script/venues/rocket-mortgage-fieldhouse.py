ffb99ec65ad56a872af1989954f762a58a79fb1c
skullface <skullface@users.noreply.github.com>
2023-09-14 13:41:44 -0400

    skip events with multiple dates

--- script/venues/rocket-mortgage-fieldhouse.py
+++ script/venues/rocket-mortgage-fieldhouse.py
@@ -50,12 +50,15 @@ for show in shows:
     all_shows_data['link'] = link.get('href')
 
     date = show.find('div', class_='date').get('aria-label')
-    date = datetime.strptime(date, '%B %d %Y')
+    if ' to ' in date:
+      continue
+    else:
+      date = datetime.strptime(date, '%B %d %Y')
 
-    all_shows_data['date'] = date.strftime('%Y-%m-%d') 
+      all_shows_data['date'] = date.strftime('%Y-%m-%d') 
 
-    all_shows_data['venue'] = 'Rocket Mortgage FieldHouse'
-    all_shows_list.append(all_shows_data)
+      all_shows_data['venue'] = 'Rocket Mortgage FieldHouse'
+      all_shows_list.append(all_shows_data)
 
 all_shows_json = json.dumps(all_shows_list, indent=2)
 print(all_shows_json)

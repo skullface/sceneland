f9b6aacdc4b40b47c7e1238bc5dd132fed278fd0
skullface <skullface@users.noreply.github.com>
2023-09-23 14:29:42 -0400

    skip beachland shows at other venues

--- script/venues/beachland.py
+++ script/venues/beachland.py
@@ -17,68 +17,69 @@ all_shows_list = []
 
 # Iterate through the events in the DOM
 for show in shows:
-  # Empty object for each show, to be populated below
-  all_shows_data = {} 
-  # Get the DOM elements containing artists
-  artist_elements = show.find_all('span', class_='tw-artist')
-  artists_list = []
-  # Loop through all the artists, add them to the object
-  for artist in artist_elements:
-    artists_list.append(artist.text.strip().replace(' - ', ': '))
-    if not artist.text.strip() == 'This Way Out':
-      all_shows_data['artist'] = artists_list
-
-  # Get all links in the show container element
-  for link_element in show.findAll('a'):
-    # Ignore all links that aren't within an `<h1>`
-    if link_element.parent.name == 'h1':
-       # Add the link to the object
-      all_shows_data['link'] = link_element['href']
-
-  # Check if “sold out” element exists
-  sold_out = show.find('span', class_='st_soldout')
-  # If it does, add a `sold_out` boolean key as 'true' to the object
-  if sold_out:
-    all_shows_data['sold_out'] = True
-
-  # Get the date and time DOM elements
-  date = show.find('span', class_='event-date').text.strip().replace('\n', ' ')
-  time = show.find('span', class_='tw-event-time').text.strip()
-
-  # Split the date string into an array with named items
-  weekday, month, day = date.split()
-  # Convert the three-letter month name into its number (1-12)
-  month_number = datetime.strptime(month, '%b').month
-
-  # Get the current date and year
-  current_date = datetime.now().date()
-  year = current_date.year
-
-  # If the event’s month is smaller than the current month, it’s next year
-  if month_number < current_date.month:
-    year = current_date.year + 1
-  else:
-    year = current_date.year
-
-  # Add the appropriate year to the given date string
-  date = date + ' ' + str(year)
-  # Take the date and time strings and interpret them as datetime object
-  date = datetime.strptime(date, '%A %b %d %Y')
-  time = datetime.strptime(time, '%I:%M %p').time()
-  # Combine them, replacing the space with a T for our ISO formatted date
-  all_shows_data['date'] = str(date).split(' ', 1)[0] + 'T' + str(time)
-
+  
   # Get the venue DOM element
   venue = show.find('span', class_='venue')
-  # Add the venue to the object
-  if venue.text.strip() == 'Beachland Ballroom':
-    all_shows_data['venue'] = venue.text.strip()
-  elif venue.text.strip() == 'Beachland Tavern':
-    all_shows_data['venue'] = venue.text.strip()
-  else:
-    all_shows_data['venue'] = ''
-  # Populate the empty array `[]` with the object we just defined
-  all_shows_list.append(all_shows_data)
+  # Check if the venue is "Beachland Ballroom" or "Beachland Tavern"
+  venue_text = venue.text.strip()
+  if venue_text == 'Beachland Ballroom' or venue_text == 'Beachland Tavern':
+
+    # Empty object for each show, to be populated below
+    all_shows_data = {} 
+    # Get the DOM elements containing artists
+    artist_elements = show.find_all('span', class_='tw-artist')
+    artists_list = []
+    # Loop through all the artists, add them to the object
+    for artist in artist_elements:
+      artists_list.append(artist.text.strip().replace(' - ', ': '))
+      if not artist.text.strip() == 'This Way Out':
+        all_shows_data['artist'] = artists_list
+
+    # Get all links in the show container element
+    for link_element in show.findAll('a'):
+      # Ignore all links that aren't within an `<h1>`
+      if link_element.parent.name == 'h1':
+        # Add the link to the object
+        all_shows_data['link'] = link_element['href']
+
+    # Check if “sold out” element exists
+    sold_out = show.find('span', class_='st_soldout')
+    # If it does, add a `sold_out` boolean key as 'true' to the object
+    if sold_out:
+      all_shows_data['sold_out'] = True
+
+    # Get the date and time DOM elements
+    date = show.find('span', class_='event-date').text.strip().replace('\n', ' ')
+    time = show.find('span', class_='tw-event-time').text.strip()
+
+    # Split the date string into an array with named items
+    weekday, month, day = date.split()
+    # Convert the three-letter month name into its number (1-12)
+    month_number = datetime.strptime(month, '%b').month
+
+    # Get the current date and year
+    current_date = datetime.now().date()
+    year = current_date.year
+
+    # If the event’s month is smaller than the current month, it’s next year
+    if month_number < current_date.month:
+      year = current_date.year + 1
+    else:
+      year = current_date.year
+
+    # Add the appropriate year to the given date string
+    date = date + ' ' + str(year)
+    # Take the date and time strings and interpret them as datetime object
+    date = datetime.strptime(date, '%A %b %d %Y')
+    time = datetime.strptime(time, '%I:%M %p').time()
+    # Combine them, replacing the space with a T for our ISO formatted date
+    all_shows_data['date'] = str(date).split(' ', 1)[0] + 'T' + str(time)
+
+    # Add the venue to the object
+    all_shows_data['venue'] = venue_text
+
+    # Populate the empty array `[]` with the object we just defined
+    all_shows_list.append(all_shows_data)
 
 # Make our final list more readable with JSON formatting
 # This is technically a Python string, but we don’t care about that anymore

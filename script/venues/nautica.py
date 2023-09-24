a22caadcf54315af0704125ff6745da2683c1e85
skullface <skullface@users.noreply.github.com>
2023-09-24 18:46:15 -0400

    skip nautica if no shows

--- script/venues/nautica.py
+++ script/venues/nautica.py
@@ -7,24 +7,29 @@ page = session.get('https://nauticawaterfrontdistrict.com/events/?venue=jacobs-p
 
 soup = BeautifulSoup(page.content, 'html.parser')
 calendar = soup.find('ul', class_='card-list')
-shows = calendar.find_all('li', class_='card-list__item')
 
-all_shows_list = []
+if calendar:
+  shows = calendar.find_all('li', class_='card-list__item')
 
-for show in shows:
-  all_shows_data = {} 
-  headliner = show.find('h5')
-  opener = show.find('h5').find_next_sibling('p')
-  if not opener:
-    all_shows_data['artist'] = [headliner.text.strip()]
-  else:
-    all_shows_data['artist'] = [headliner.text.strip() + opener.text.strip().replace('with ', ', ').replace(', and ', ', ')]
-  for link_element in show.findAll('a'):
-    all_shows_data['link'] = link_element['href']
-  date = show.find('span', class_='event-start')
-  all_shows_data['date'] = date.get('content')
-  all_shows_data['venue'] = 'Jacobs Pavilion at Nautica'
-  all_shows_list.append(all_shows_data)
+  all_shows_list = []
 
-all_shows_json = json.dumps(all_shows_list, indent=2) 
-print(all_shows_json)
+  for show in shows:
+    all_shows_data = {} 
+    headliner = show.find('h5')
+    opener = show.find('h5').find_next_sibling('p')
+    if not opener:
+      all_shows_data['artist'] = [headliner.text.strip()]
+    else:
+      all_shows_data['artist'] = [headliner.text.strip() + opener.text.strip().replace('with ', ', ').replace(', and ', ', ')]
+    for link_element in show.findAll('a'):
+      all_shows_data['link'] = link_element['href']
+    date = show.find('span', class_='event-start')
+    all_shows_data['date'] = date.get('content')
+    all_shows_data['venue'] = 'Jacobs Pavilion at Nautica'
+    all_shows_list.append(all_shows_data)
+
+  all_shows_json = json.dumps(all_shows_list, indent=2) 
+  print(all_shows_json)
+else:
+  all_shows_json = '[]'
+  print(all_shows_json)
